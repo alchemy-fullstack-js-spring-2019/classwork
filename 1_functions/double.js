850d4f15e0330a289abc01425efd0ebc15edf58b
Ryan Mehta <ryan.mehta@gmail.com>
2019-04-01 10:40:58 -0700

    init

---
+++ 1_functions/double.js
@@ -0,0 +1,6 @@
+function double(n) {
+  return n * 2;
+}
+
+// export default double;
+module.exports = double;
---
+++ 1_functions/double.test.js
@@ -0,0 +1,14 @@
+// import double from './double'
+const double = require('./double');
+
+describe('double function', () => {
+  it('doubles the number 2', () => {
+    const result = double(2);
+    expect(result).toEqual(4);
+  });
+
+  it('doubles the number 5', () => {
+    const result = double(5);
+    expect(result).toEqual(10);
+  });
+});
