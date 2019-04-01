43e280806a7837376f210ee41d809dbedfbb37a9
Ryan Mehta <ryan.mehta@gmail.com>
2019-04-01 11:12:15 -0700

    double array

--- 1_functions/double.js
+++ 1_functions/double.js
@@ -2,5 +2,13 @@ function double(n) {
   return n * 2;
 }
 
+function doubleArray(arr) {
+  return arr.map(double);
+}
+
+// export const doubleArray = () => {}
 // export default double;
-module.exports = double;
+module.exports = {
+  double,
+  doubleArray
+};
