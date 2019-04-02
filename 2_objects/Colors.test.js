957cd4394865e9903a03c28037c0b1583e0f098c
Ryan Mehta <ryan.mehta@gmail.com>
2019-04-02 15:20:49 -0700

    colors

---
+++ 2_objects/Colors.test.js
@@ -0,0 +1,23 @@
+const Colors = require('./Colors');
+
+describe('Colors', () => {
+  let colors = null;
+  beforeEach(() => {
+    colors = new Colors();
+  });
+
+  it('has a list of favorite colors', () => {
+    expect(colors.favorites).toEqual([]);
+  });
+
+  it('can add new favorites with addColor', () => {
+    colors.addColor('red');
+    expect(colors.favorites).toContainEqual('red');
+  });
+
+  it('can check if favorites hasColor', () => {
+    colors.addColor('red');
+    expect(colors.hasColor('red')).toBeTruthy();
+    expect(colors.hasColor('blue')).toBeFalsy();
+  });
+});
---
+++ 2_objects/uuid.js
@@ -0,0 +1,11 @@
+const uuid = require('uuid/v4');
+
+expect(uuid()).toEqual(expect.any(String));
+
+console.log(uuid());
+console.log(uuid());
+console.log(uuid());
+console.log(uuid());
+console.log(uuid());
+console.log(uuid());
+console.log(uuid());
