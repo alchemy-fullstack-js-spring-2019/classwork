55aa23c8cd2788cbb7def3c6ba05a76ebb766ed9
Ryan Mehta <ryan.mehta@gmail.com>
2019-04-10 13:28:58 -0700

    read promise

--- 8_promises/files.js
+++ 8_promises/files.js
@@ -1,4 +1,31 @@
 const fsPromises = require('fs').promises;
+const fs = require('fs');
+
+function readFilePromise(src) {
+  return new Promise((resolve, reject) => {
+    fs.readFile(src, { encoding: 'utf8' }, (err, data) => {
+      if(err) return reject(err);
+
+      resolve(data);
+    });
+  });
+}
+
+readFilePromise('./1_promises.md')
+  .then(data => console.log('success', data))
+  .catch(err => console.error('error', err));
+
+
+// const readFilePromise = new Promise((resolve, reject) => {
+//   fs.readFile('./1_promises.md', { encoding: 'utf8' }, (err, data) => {
+//     if (err) return reject(err);
+
+//     resolve(data);
+//   });
+// });
+
+
+
 
 
 // fsPromises.readFile('./1_promises.md', { encoding: 'utf8' })
@@ -24,7 +51,13 @@ const fsPromises = require('fs').promises;
 
 
 // dog API
-Promise.resolve({
-  name: 'spot',
-  age: 10
-});
+// Promise.resolve({
+//   name: 'spot',
+//   age: 10
+// });
+
+// fsPromises.readFile('./1_promises.md', { encoding: 'utf8' })
+//   .then(data => data.replace(/[A-Z]/g, ''))
+//   .then(noCaps => noCaps.toUpperCase())
+//   .then(allCaps => [...allCaps].reverse().join(''))
+//   .then(console.log);
