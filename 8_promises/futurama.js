edf507ccc5cc7a4ba5b2668c55f045aa790353b1
Ryan Mehta <ryan.mehta@gmail.com>
2019-04-10 16:24:23 -0700

    promise all

--- 8_promises/futurama.js
+++ 8_promises/futurama.js
@@ -1,7 +1,15 @@
 const request = require('superagent');
 
-request
-  .get('http://url')
-  .then(res => {
-    res.body;
-  });
+// POST - create a new thing
+// GET - get a thing
+// PUT - update a thing in its entirety (put it there)
+// PATCH - partially update thing (patch some of it)
+// DELETE - delete a thing
+function getCharacterQuotes(character, count = 1) {
+  return request
+    .get(`http://futuramaapi.herokuapp.com/api/characters/${character}/${count}`)
+    .then(res => res.body);
+}
+
+getCharacterQuotes('Bender', 5)
+  .then(console.log);
