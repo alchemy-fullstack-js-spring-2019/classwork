595d659903737fd04561a501df01ef33d6e07a3c
Ryan Mehta <ryan.mehta@gmail.com>
2019-04-15 11:37:13 -0700

    404

--- 11_express/lib/app.js
+++ 11_express/lib/app.js
@@ -17,4 +17,8 @@ app.use(express.json());
 // go to tweetsRoutes
 app.use('/tweets', tweetsRoutes);
 
+app.use((req, res, next) => {
+  res.status(404).send({ error: 'Not Found' });
+});
+
 module.exports = app;
