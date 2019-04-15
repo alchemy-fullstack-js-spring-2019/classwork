a3dc094f3d297e83fd093357d3a94b9667f80b3f
Ryan Mehta <ryan.mehta@gmail.com>
2019-04-15 11:32:31 -0700

    loggin middleware

--- 11_express/lib/app.js
+++ 11_express/lib/app.js
@@ -6,7 +6,7 @@ const tweetsRoutes = require('./routes/tweets');
 // Between an incoming request
 // and an outgoing response
 app.use((req, res, next) => {
-  console.log('you are in middleware');
+  console.log(`[${req.method}] ${req.url}`);
   next();
 });
 
