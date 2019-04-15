6037f2e9fa843346b04ff896078d6adb162db75f
Ryan Mehta <ryan.mehta@gmail.com>
2019-04-15 10:41:12 -0700

    finish routes

--- 11_express/lib/routes/tweets.js
+++ 11_express/lib/routes/tweets.js
@@ -13,4 +13,37 @@ module.exports = Router()
       .then(createdTweet => {
         res.send(createdTweet);
       });
+  })
+
+  .get('/', (req, res) => {
+    Tweet
+      .find()
+      .then(tweets => res.send(tweets));
+  })
+
+  .get('/:id', (req, res) => {
+    const { id } = req.params;
+    Tweet
+      .findById(id)
+      .then(tweet => res.send(tweet));
+  })
+
+  .put('/:id', (req, res) => {
+    const { id } = req.params;
+    const {
+      handle,
+      body
+    } = req.body;
+
+    Tweet
+      .findByIdAndUpdate(id, { handle, body })
+      .then(updatedTweet => res.send(updatedTweet));
+  })
+
+  .delete('/:id', (req, res) => {
+    const { id } = req.params;
+
+    Tweet
+      .findByIdAndDelete(id)
+      .then(result => res.send(result));
   });
--- 11_express/test/app.test.js
+++ 11_express/test/app.test.js
@@ -1,7 +1,13 @@
 const request = require('supertest');
 const app = require('../lib/app');
+const Tweet = require('../lib/models/Tweet');
 
 describe('app routes', () => {
+  afterEach(() => {
+    return Tweet.drop();
+  });
+
+  // CRUD
   it('can create a new tweet', () => {
     return request(app)
       .post('/tweets')
@@ -14,6 +20,66 @@ describe('app routes', () => {
           handle: 'ryan',
           body: 'my first tweet',
           _id: expect.any(String)
+        });
+      });
+  });
+
+  it('can get a list of tweets', () => {
+    return Tweet
+      .create({ handle: 'ryan', body: 'my tweet' })
+      .then(() => {
+        return request(app)
+          .get('/tweets');
+      })
+      .then(res => {
+        expect(res.body).toHaveLength(1);
+      });
+  });
+
+  it('can get a tweet by id', () => {
+    return Tweet
+      .create({ handle: 'ryan', body: 'my tweet' })
+      .then(createdTweet => {
+        return request(app)
+          .get(`/tweets/${createdTweet._id}`);
+      })
+      .then(res => {
+        expect(res.body).toEqual({
+          handle: 'ryan',
+          body: 'my tweet',
+          _id: expect.any(String)
+        });
+      });
+  });
+
+  it('can update a tweet by id', () => {
+    return Tweet.create({ handle: 'ryan', body: 'my tweet' })
+      .then(tweet => {
+        return request(app)
+          .put(`/tweets/${tweet._id}`)
+          .send({
+            handle: 'ryan',
+            body: 'my good tweet'
+          });
+      })
+      .then(res => {
+        expect(res.body).toEqual({
+          handle: 'ryan',
+          body: 'my good tweet',
+          _id: expect.any(String)
+        });
+      });
+  });
+
+  it('can delete a tweet by id', () => {
+    return Tweet.create({ handle: 'ryan', body: 'my tweet' })
+      .then(tweet => {
+        return request(app)
+          .delete(`/tweets/${tweet._id}`);
+      })
+      .then(res => {
+        expect(res.body).toEqual({
+          deleted: 1
         });
       });
   });
