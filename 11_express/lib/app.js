fa514c7eaadcf97af6e24cbdc61293784d6f26c7
Ryan Mehta <ryan.mehta@gmail.com>
2019-04-15 14:39:32 -0700

    adv logger

--- 11_express/lib/app.js
+++ 11_express/lib/app.js
@@ -6,8 +6,13 @@ const tweetsRoutes = require('./routes/tweets');
 // Between an incoming request
 // and an outgoing response
 app.use((req, res, next) => {
-  console.log(`[${req.method}] ${req.url}`);
-  next('NOPE!!!');
+  // time in ms
+  const startAt = Date.now();
+  res.on('finish', () => {
+    const totalTime = Date.now() - startAt;
+    console.log(`[${req.method}] ${req.baseUrl} ${res.statusCode} - ${totalTime}ms`);
+  });
+  next();
 });
 
 // Body Parser
