6e3721ddf2bbede577827437c796d0e0315e3de5
Ryan Mehta <ryan.mehta@gmail.com>
2019-04-16 13:25:41 -0700

    tweet model

---
+++ 12_mongodb/test/app.test.js
@@ -0,0 +1,19 @@
+const mongoose = require('mongoose');
+const request = require('supertest');
+const app = require('../lib/app');
+
+describe('tweet routes', () => {
+  beforeAll(() => {
+    return mongoose.connect('mongodb://localhost:27107/tweets', {
+      useNewUrlParser: true
+    });
+  });
+
+  beforeEach(() => {
+    return mongoose.connection.dropDatabase();
+  });
+
+  afterAll(() => {
+    return mongoose.connection.close();
+  });
+});
---
+++ 12_mongodb/test/models/Tweet.test.js
@@ -0,0 +1,50 @@
+const mongoose = require('mongoose');
+const Tweet = require('../../lib/models/Tweet');
+
+describe('Tweet model', () => {
+  it('has a handle and body field', () => {
+    const tweet = new Tweet({
+      handle: 'ryan',
+      body: 'my first tweet'
+    });
+
+    expect(tweet.toJSON()).toEqual({
+      handle: 'ryan',
+      body: 'my first tweet',
+      _id: expect.any(mongoose.Types.ObjectId)
+    });
+  });
+
+  it('has a required handle field', () => {
+    const tweet = new Tweet({
+      body: 'my first tweet'
+    });
+
+    const errors = tweet.validateSync().errors;
+
+    expect(errors.handle.message).toEqual('Path `handle` is required.');
+  });
+
+  it('has a required body field', () => {
+    const tweet = new Tweet({
+      handle: 'ryan'
+    });
+
+    const errors = tweet.validateSync().errors;
+
+    expect(errors.body.message).toEqual('Path `body` is required.');
+  });
+
+  it('has a body with maxlength 256', () => {
+    const body = 'a'.repeat(300);
+    const tweet = new Tweet({
+      handle: 'ryan',
+      body
+    });
+
+    const errors = tweet.validateSync().errors;
+
+    expect(errors.body.message).toEqual(`Path \`body\` (\`${body}\`) is longer than the maximum allowed length (256).`);
+  });
+
+});
