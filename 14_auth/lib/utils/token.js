27ba7455ab5346d46154e62a47e2c1b73bc4b2ea
Ryan Mehta <ryan.mehta@gmail.com>
2019-04-22 15:31:25 -0700

    tokenize

--- 14_auth/lib/utils/token.js
+++ 14_auth/lib/utils/token.js
@@ -1,9 +1,19 @@
 const jwt = require('jsonwebtoken');
 
+const EXPIRES_IN = '1d';
+
 const tokenize = payload => {
-  process.env.AUTH_SECRET;
+  return jwt.sign(
+    { payload },
+    process.env.AUTH_SECRET,
+    { expiresIn: EXPIRES_IN });
+};
+
+const untokenize = token => {
+  // return the payload from the token
 };
 
 module.exports = {
-  tokenize
+  tokenize,
+  untokenize
 };
--- 14_auth/test/utils/token.test.js
+++ 14_auth/test/utils/token.test.js
@@ -1,5 +1,6 @@
 require('dotenv').config();
 const { tokenize } = require('../../lib/utils/token');
+const jwt = require('jsonwebtoken');
 
 describe('jwt token', () => {
   it('can create a token', () => {
@@ -9,5 +10,20 @@ describe('jwt token', () => {
     });
 
     expect(token).toEqual(expect.any(String));
+  });
+
+  it('can verify a token', () => {
+    const token = tokenize({
+      name: 'spot',
+      age: 12
+    });
+
+    const obj = jwt.verify(token, process.env.AUTH_SECRET);
+
+    expect(obj.payload).toEqual({
+      name: 'spot',
+      age: 12
+    });
+
   });
 });
