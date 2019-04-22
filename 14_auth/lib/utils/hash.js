53daabaea1a3fa900636547b88ba012fed097228
Ryan Mehta <ryan.mehta@gmail.com>
2019-04-22 14:47:25 -0700

    start compare

--- 14_auth/lib/utils/hash.js
+++ 14_auth/lib/utils/hash.js
@@ -1,3 +1,12 @@
-const hash = password => {
+const bcrypt = require('bcryptjs');
 
+const SALT_ROUNDS = 10;
+
+const hash = password => {
+  return bcrypt.hash(password, SALT_ROUNDS);
+};
+
+
+module.exports = {
+  hash
 };
